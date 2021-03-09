ceee3619a10395851deab23b82c38745e5acaf9a
fx67ll <2717745014@qq.com>
2021-03-09 12:11:56 +0800

    测试动态形变操作

--- three-project/medical-model/src/views/index.vue
+++ three-project/medical-model/src/views/index.vue
@@ -1,5 +1,8 @@
 <template>
-	<div id="model-container" class="model-box"></div>
+	<div class="model-box">
+		<div id="model-container" class="three-box"></div>
+		<div class="tool-box"><el-slider v-model="mouseValue" :min="0" :max="1" :step="0.1" show-input @change="setMouseVal()"></el-slider></div>
+	</div>
 </template>
 
 <script>
@@ -11,122 +14,141 @@ import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
 export default {
 	name: 'modelIndex',
 	data() {
-		return {};
+		return {
+			// 控制人物模型的张口大小
+			mouseValue: 0,
+			// 相机对象
+			camera: null,
+			// 场景对象
+			scene: null,
+			// 渲染器对象
+			renderer: null,
+			// 状态对象
+			stats: null,
+			// 混淆器对象？？？
+			mixer: null,
+			// 时间对象
+			clock: null,
+			// FBX模型加载对象
+			fbxloader: null
+		};
 	},
 	mounted() {
 		this.modelInit();
 	},
 	methods: {
+		// 修改变形数据
+		setMouseVal(value) {
+			console.log(this.mouseValue)
+			console.log(this.scene.children[4].children[0].morphTargetInfluences[2]);
+			this.scene.children[4].children[0].morphTargetInfluences[2] = this.mouseValue;
+		},
 		modelInit() {
-			let camera, scene, renderer, stats;
+			this.clock = new THREE.Clock();
+			this.fbxloader = new FBXLoader();
+			this.init();
+			this.animate();
+		},
+		init() {
+			let self = this;
 
-			const clock = new THREE.Clock();
+			// 模型加载的容器
+			const container = document.getElementById('model-container');
 
-			let mixer;
+			self.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
+			self.camera.position.set(100, 200, 300);
 
-			init();
-			animate();
+			self.scene = new THREE.Scene();
+			self.scene.background = new THREE.Color(0xa0a0a0);
+			self.scene.fog = new THREE.Fog(0xa0a0a0, 200, 1000);
 
-			function init() {
-				// 模型加载的容器
-				const container = document.getElementById('model-container');
+			// 创建半球光源用于模拟真实环境
+			const ambientLight = new THREE.AmbientLight('#FFFFFF');
+			self.scene.add(ambientLight);
 
-				camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
-				camera.position.set(100, 200, 300);
+			// 设置平行光赖添加模型阴影
+			const dirLight = new THREE.DirectionalLight(0xffffff);
+			dirLight.position.set(0, 200, 100);
+			dirLight.castShadow = true;
+			dirLight.shadow.camera.top = 180;
+			dirLight.shadow.camera.bottom = -100;
+			dirLight.shadow.camera.left = -120;
+			dirLight.shadow.camera.right = 120;
+			self.scene.add(dirLight);
 
-				scene = new THREE.Scene();
-				scene.background = new THREE.Color(0xa0a0a0);
-				scene.fog = new THREE.Fog(0xa0a0a0, 200, 1000);
-				
-				// 创建半球光源用于模拟真实环境
-				const ambientLight = new THREE.AmbientLight(0x0c0c0c);
-				scene.add(ambientLight); 
-				
-				// 设置平行光赖添加模型阴影
-				const dirLight = new THREE.DirectionalLight(0xffffff);
-				dirLight.position.set(0, 200, 100);
-				dirLight.castShadow = true;
-				dirLight.shadow.camera.top = 180;
-				dirLight.shadow.camera.bottom = -100;
-				dirLight.shadow.camera.left = -120;
-				dirLight.shadow.camera.right = 120;
-				scene.add(dirLight);
+			// 添加光线辅助线
+			// self.scene.add( new THREE.CameraHelper( dirLight.shadow.self.camera ) );
 
-				// 添加光线辅助线
-				// scene.add( new THREE.CameraHelper( dirLight.shadow.camera ) );
+			// ground
+			const mesh = new THREE.Mesh(new THREE.PlaneGeometry(2000, 2000), new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false }));
+			mesh.rotation.x = -Math.PI / 2;
+			mesh.receiveShadow = true;
+			self.scene.add(mesh);
 
-				// ground
-				const mesh = new THREE.Mesh(new THREE.PlaneGeometry(2000, 2000), new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false }));
-				mesh.rotation.x = -Math.PI / 2;
-				mesh.receiveShadow = true;
-				scene.add(mesh);
+			const grid = new THREE.GridHelper(2000, 20, 0x000000, 0x000000);
+			grid.material.opacity = 0.2;
+			grid.material.transparent = true;
+			self.scene.add(grid);
 
-				const grid = new THREE.GridHelper(2000, 20, 0x000000, 0x000000);
-				grid.material.opacity = 0.2;
-				grid.material.transparent = true;
-				scene.add(grid);
+			// 使用FBXLoader加载模型
+			self.fbxloader.load('models/Human.fbx', function(object) {
+				self.mixer = new THREE.AnimationMixer(object);
 
-				// model
-				const loader = new FBXLoader();
-				loader.load('models/ceshi.fbx', function(object) {
-					mixer = new THREE.AnimationMixer(object);
-					
-					console.log(object.children[0].geometry.morphAttributes)
-					console.log(object.children[0].morphTargetInfluences[0])
-					object.children[0].morphTargetInfluences[0] = 0 // 前升
-					object.children[0].morphTargetInfluences[1] = 1 // 张嘴
-					object.children[0].morphTargetInfluences[2] = 0.9 // 抬头
-					
-					// const action = mixer.clipAction(object.animations[0]);
-					// action.play();
+				// console.log(object.children[0].geometry.morphAttributes);
+				// console.log(object.children[0].morphTargetInfluences[0]);
+				object.children[0].morphTargetInfluences[0] = 0; // 前升
+				object.children[0].morphTargetInfluences[1] = 1; // 张嘴
+				object.children[0].morphTargetInfluences[2] = 0.9; // 抬头
 
-					object.traverse(function(child) {
-						if (child.isMesh) {
-							child.castShadow = true;
-							child.receiveShadow = true;
-						}
-					});
+				// const action = self.mixer.clipAction(object.animations[0]);
+				// action.play();
 
-					scene.add(object);
+				object.traverse(function(child) {
+					if (child.isMesh) {
+						child.castShadow = true;
+						child.receiveShadow = true;
+					}
 				});
 
-				renderer = new THREE.WebGLRenderer({ antialias: true });
-				renderer.setPixelRatio(window.devicePixelRatio);
-				renderer.setSize(window.innerWidth, window.innerHeight);
-				renderer.shadowMap.enabled = true;
-				container.appendChild(renderer.domElement);
+				self.scene.add(object);
+			});
 
-				const controls = new OrbitControls(camera, renderer.domElement);
-				controls.target.set(0, -50, 0);
-				controls.update();
+			self.renderer = new THREE.WebGLRenderer({ antialias: true });
+			self.renderer.setPixelRatio(window.devicePixelRatio);
+			self.renderer.setSize(window.innerWidth, window.innerHeight);
+			self.renderer.shadowMap.enabled = true;
+			container.appendChild(self.renderer.domElement);
 
-				window.addEventListener('resize', onWindowResize);
+			const controls = new OrbitControls(self.camera, self.renderer.domElement);
+			controls.target.set(0, -50, 0);
+			controls.update();
 
-				// stats
-				stats = new Stats();
-				container.appendChild(stats.dom);
-			}
+			window.addEventListener('resize', self.onWindowResize);
 
-			function onWindowResize() {
-				camera.aspect = window.innerWidth / window.innerHeight;
-				camera.updateProjectionMatrix();
+			// 帧率监测模块引入
+			self.stats = new Stats();
+			container.appendChild(self.stats.dom);
+		},
+		onWindowResize() {
+			this.camera.aspect = window.innerWidth / window.innerHeight;
+			this.camera.updateProjectionMatrix();
 
-				renderer.setSize(window.innerWidth, window.innerHeight);
-			}
+			this.renderer.setSize(window.innerWidth, window.innerHeight);
+		},
+		animate() {
+			let self = this;
 
-			//
+			requestAnimationFrame(self.animate);
 
-			function animate() {
-				requestAnimationFrame(animate);
+			// 获得前后两次执行的时间间隔
+			const delta = self.clock.getDelta();
 
-				const delta = clock.getDelta();
+			if (self.mixer) self.mixer.update(delta);
 
-				if (mixer) mixer.update(delta);
+			self.renderer.render(self.scene, self.camera);
 
-				renderer.render(scene, camera);
-
-				stats.update();
-			}
+			// 更新帧率监测
+			self.stats.update();
 		}
 	}
 };
@@ -136,9 +158,18 @@ export default {
 .model-box {
 	width: 100%;
 	height: 100%;
-	// width: calc(~"100% - 2px");
-	// height: calc(~"100% - 2px");
-	// border: 1px solid red;
 	.ban-user-select();
+	.three-box {
+		width: 100%;
+		height: 100%;
+	}
+	.tool-box {
+		width: 400px;
+		border: 1px solid red;
+		padding: 20px;
+		position: absolute;
+		top: 30px;
+		right: 30px;
+	}
 }
 </style>
